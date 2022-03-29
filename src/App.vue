d7fbc9fe5c757c59316d6b2d8c409ed789bb1002
dymafr <erwan1309@hotmail.com>
2022-03-29 11:17:33 +0200

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="p-20">
     <button class="btn btn-primary" @click="show = !show">Display</button>
-    <h1 v-if="show">Hello World !</h1>
+    <Transition appear name="fade">
+      <h1 v-if="show">Bonjour !</h1>
+    </Transition>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const show = ref(false);
+const show = ref(true);
 </script>
 
 <style lang="scss">
 @import './assets/scss/base.scss';
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 2s;
+}
 </style>
